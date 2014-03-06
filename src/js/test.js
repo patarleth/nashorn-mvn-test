4f3b737cc05e0cc54bcfbd7227db9731afa8fd42
Pat Arleth <arleth@about.me>
2014-03-06 09:12:27 -0500

    example js plugin usage

---
+++ src/js/test.js
@@ -0,0 +1,1 @@
+java.lang.System.out.println('\n\n\n\nhello from javascript exec\'d as a plugin\n\n\n');
---
+++ src/js/testworld.js
@@ -0,0 +1,1 @@
+java.lang.System.out.println('\n\n\n\n<phase>test</phase>\nTEST WORLD FROM .js FILE\n\n\n');
